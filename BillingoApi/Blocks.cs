4363bcfedad683007c595859caad8156b9ca38c7
Attila Köteles <attila.koteles@gmail.com>
2018-05-19 15:04:45 +0200

    Adding Blocks class also

---
+++ BillingoApi/Blocks.cs
@@ -0,0 +1,23 @@
+﻿using BillingoApi.Core;
+using BillingoApi.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BillingoApi
+{
+    public class Blocks
+    {
+        readonly Connection connection;
+
+        public Blocks(string publicKey, string privateKey)
+        {
+            connection = new Connection(publicKey, privateKey);
+        }
+
+        public async Task<List<Block>> LoadAllAsync(int limit = 50)
+        {
+            var l = new DataLoader<Block>(connection, "/invoices/blocks");
+            return await l.FetchAllAsync(limit);
+        }
+    }
+}
