ce4c93e735c035400658155f7a9060fc28eb4e72
Attila Köteles <attila.koteles@gmail.com>
2019-10-15 09:02:35 +0200

    New release 1.0.2
    New function: Invoices.FetchOneInvoice
    New optional parameters for Invoices.QueryAsync

--- BillingoApi/Invoices.cs
+++ BillingoApi/Invoices.cs
@@ -23,8 +23,16 @@ namespace BillingoApi
         /// <param name="limit">optional, limit of the results, defaults to 50</param>
         /// <param name="yearStart">optional, which year to list, defaults to current year</param>
         /// <param name="startDateAsString">optional, list invoices from this date, example format: "2018-01-01", defaults to "2010-01-01"</param>
+        /// <param name="toInvoiceNo">optional, the final invoice number to return</param>
+        /// <param name="yearEnd">optional, the final end year of invoice</param>
         /// <returns></returns>
-        public async Task<List<Invoice>> QueryAsync(int fromInvoiceNo = -1, long blockId = -1, int limit = 50, int yearStart = -1, string startDateAsString = "2010-01-01")
+        public async Task<List<Invoice>> QueryAsync(int fromInvoiceNo = -1,
+                                                    long blockId = -1,
+                                                    int limit = 50,
+                                                    int yearStart = -1,
+                                                    string startDateAsString = "2010-01-01",
+                                                    int toInvoiceNo = -1,
+                                                    int yearEnd = -1)
         {
             var l = new DataLoader<Invoice>(connection, "/invoices/query");
 
@@ -41,7 +49,31 @@ namespace BillingoApi
             if (fromInvoiceNo > -1)
                 qp["num_start"] = fromInvoiceNo.ToString(); // fromInvoiceNo - inclusive - this will be the first invoice to return
 
+            if (toInvoiceNo > -1)
+                qp["num_end"] = toInvoiceNo.ToString();
+
+            if (yearEnd > -1)
+                qp["year_end"] = yearEnd.ToString();
+
             return await l.FetchAllAsync(limit);
         }
+
+        public async Task<Invoice> FetchOneInvoice(long blockUid, int year, int invoiceNo)
+        {
+            var invoices = await QueryAsync(
+                blockId: blockUid,
+                fromInvoiceNo: invoiceNo,
+                toInvoiceNo: invoiceNo,
+                yearStart: year,
+                yearEnd: year,
+                limit: 1);
+
+            if (invoices.Count != 1)
+                return null;
+
+            return invoices[0];
+        }
+
+
     }
 }
